113e7f878079d8d25f242ec1c1f0533382625671
Morten Wærsland <morten_w92@hotmail.com>
2015-11-03 09:20:54 +0100

    Restrukturert koden.

--- COMMON_PRE.m
+++ COMMON_PRE.m
@@ -1,20 +1,39 @@
 function [fire, transition] = COMMON_PRE(transition)
 %get rith value from token input
 global global_info;
-global_info.counter = 0;
+
+colors = global_info.colors;
 
 if strcmp(transition.name, 'tRemove'),
     transition.override = 1;
     fire = 1;
     return;
-elseif strcmp(transition.name, 'tInit'),
-    global_info.counter = global_info.counter + 1;
     
-    %for n = 1:4
-    tokID = tokenAnyColor('pTask',1,{'at:1'});
+elseif strcmp(transition.name, 'tInit'),
+    at = ['at:', int2str(global_info.counter)];
+    tokID = tokenAnyColor('pTask', 1, at);
+    if eq(tokID, 0.0),
+        global_info.counter = global_info.counter + 1;
+        fire = 0;
+        disp('null');
+        return;
+    end;
+    disp('not null');
     disp(tokID);
     fire = (tokID);
-    %end;
+    return;
+elseif strcmp(transition.name, 'tColorizer'),
+    tColorizer = get_trans('tColorizer');
+    index = tColorizer.times_fired;
+    i = index + 1;
+
+    at = colors{i}{1};
+    unit_id = colors{i}{2};
+    total = colors{i}{3};
+    job_id = colors{i}{4};
+
+    transition.new_color = {at, unit_id, total, job_id};
+    fire = 1;
     return;
 end;
 fire = 1;
--- scheduler_msf.m
+++ scheduler_msf.m
@@ -1,10 +1,18 @@
 clear all; clc;
-%global global_info;
+global global_info;
 %global_info.TOKEN_FIRING_TIME = [];
+global_info.counter = 1;
+global_info.counter_processor = 1;
 
+global_info.colors = {{'at:1', 'unit_id:1', 'total:3','job_id:1'},...
+ {'at:1', 'unit_id:2', 'total:3','job_id:1'},...
+ {'at:1', 'unit_id:3', 'total:3','job_id:1'},...
+{'at:2', 'unit_id:1', 'total:2','job_id:2'},...
+{'at:2', 'unit_id:2', 'total:2','job_id:2'}};
 
 pns = pnstruct('generator_pdf');
 dyn.m0 = {'pJobbUnits', 5, 'pTrigger',1}; % pJobbUnits starts with 5 tokens
+dyn.ft = {'allothers',1};
 pni = initialdynamics(pns, dyn);
 
 sim = gpensim(pni);
