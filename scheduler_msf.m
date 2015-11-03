23666048062cab4caa18fb8f4013dd338f386d6a
sandves <stian@sandve.org>
2015-11-03 11:57:58 +0100

    SJF implemented

--- scheduler_msf.m
+++ scheduler_msf.m
@@ -10,7 +10,9 @@ global_info.colors = {{'at:1', 'unit_id:1', 'total:3','job_id:1'},...
 {'at:2', 'unit_id:1', 'total:2','job_id:2'},...
 {'at:2', 'unit_id:2', 'total:2','job_id:2'}};
 
-global_info.algorithm = 'fcfs';
+global_info.remaining_units = sort_on_length(global_info.colors);
+
+global_info.algorithm = 'sjf';
 
 pns = pnstruct('generator_pdf');
 dyn.m0 = {'pJobbUnits', 5, 'pTrigger',1}; % pJobbUnits starts with 5 tokens
--- tInit_pre.m
+++ tInit_pre.m
@@ -1,29 +1,42 @@
 function [fire, transition] = tInit_pre(transition)
     global global_info;
+    
+    % Do not fire until all job units are colorized.
+    pJobUnits = get_place('pJobbUnits');
+    if (pJobUnits.tokens),
+        fire = 0;
+        return;
+    end
+    
     if strcmp(global_info.algorithm, 'fcfs')
-        
-        pJobUnits = get_place('pJobbUnits');
-        if not(pJobUnits.tokens),
-            fire = 0;
-            return;
-        end;
         i = 1;
         while(i),
-            at = ['at:', int2str(global_info.counter)];
+            at = ['at:', int2str(i)];
             fire = tokenAnyColor('pTask', 1, at);
             if (fire),
                 i = 0;
-                %transition.selected_tokens = fire;
+                transition.selected_tokens = fire;
             else
                 i = i + 1;
-            end;
-        end;
+            end
+        end
     elseif strcmp(global_info.algorithm, 'sjf')
-        disp('SJF not yet implemented');
-        fire = 0;
+        i = 1;
+        while(i),
+            units = global_info.remaining_units;
+            shortest_job = units(1);
+            % Remove the first job unit in the list of remaining units.
+            global_info.remaining_units = units(2:length(units));
+            total = ['total:', int2str(shortest_job)];
+            fire = tokenAnyColor('pTask', 1, total);
+            if (fire),
+                i = 0;
+                transition.selected_tokens = fire;
+            end
+        end
     elseif strcmp(global_info.algorithm, 'rr')
         disp('RR not yet implemented');
         fire = 0;
     else
         fire = 0;
-    end;
+    end
