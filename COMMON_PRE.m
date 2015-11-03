4c5748046f5c42bdbac7397cb85de8d55cb2c16a
sandves <stian@sandve.org>
2015-11-03 10:53:19 +0100

    Ready to implement the rest of the models

--- COMMON_PRE.m
+++ COMMON_PRE.m
@@ -8,24 +8,6 @@ if strcmp(transition.name, 'tRemove'),
     transition.override = 1;
     fire = 1;
     return;
-    
-elseif strcmp(transition.name, 'tInit'),
-    pJobUnits = get_place('pJobbUnits');
-    if (pJobUnits.tokens),
-        fire = 0;
-        return;
-    end;
-    i = 1;
-    while(i),
-        at = ['at:', int2str(global_info.counter)];
-        fire = tokenAnyColor('pTask', 1, at);
-        if (fire),
-            i = 0;
-            %transition.selected_tokens = fire;
-        else
-            i = i + 1;
-        end;
-    end;
 elseif strcmp(transition.name, 'tColorizer'),
     tColorizer = get_trans('tColorizer');
     index = tColorizer.times_fired;
--- tInit_pre.m
+++ tInit_pre.m
@@ -1,4 +1,23 @@
 function [fire, transition] = tInit_pre(transition)
-%get rith value from token input
-%transition.TokenAnyColor = {''};
-fire = 1;
+    global global_info;
+    if strcmp(global_info.algorithm, 'fcfs')
+        
+        pJobUnits = get_place('pJobbUnits');
+        if not(pJobUnits.tokens),
+            fire = 0;
+            return;
+        end;
+        i = 1;
+        while(i),
+            at = ['at:', int2str(global_info.counter)];
+            fire = tokenAnyColor('pTask', 1, at);
+            if (fire),
+                i = 0;
+                %transition.selected_tokens = fire;
+            else
+                i = i + 1;
+            end;
+        end;
+    else
+        fire = 0;
+    end;
