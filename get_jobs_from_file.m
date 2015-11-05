669cc121ba35404f61805bc985df55fc71b8a3dd
sandves <stian@sandve.org>
2015-11-05 16:55:29 +0100

    function to retrieve jobs from file

---
+++ get_jobs_from_file.m
@@ -0,0 +1,13 @@
+function jobs = get_jobs_from_file(path)
+    job_units = dlmread(path, '\t');
+    [rows, ~] = size(job_units);
+    jobs = cell(1, rows);
+    for row = 1 : rows
+        at = num2str(job_units(row, 1));
+        unit_id = num2str(job_units(row, 2));
+        total = num2str(job_units(row, 3));
+        job_id = num2str(job_units(row, 4));
+        color = {strcat('at:', at), strcat('unit_id:', unit_id), strcat('total:', total), strcat('job_id:', job_id)};
+        jobs{row} = color;
+    end
+end
