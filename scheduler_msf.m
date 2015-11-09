fa963dc6e3e871feb1952e3171902f4ea6548955
sandves <stian@sandve.org>
2015-11-09 14:50:41 +0100

    Removed comment

--- scheduler_msf.m
+++ scheduler_msf.m
@@ -1,6 +1,6 @@
 clear all; clc;
 global global_info;
-global_info.STOP_AT = 200;
+global_info.STOP_AT = 500;
 global_info.counter = 1;
 global_info.counter_processor = 1;
 global_info.job_id = 1;
@@ -22,17 +22,17 @@ global_info.colors = {{'at:1', 'unit_id:1', 'total:3','job_id:1'},...
 {'at:4', 'unit_id:5', 'total:5','job_id:4'}};
 %}
 disp('Loading jobs from file ...');
-global_info.colors = get_jobs_from_file('job_generator/jobs_test.txt');
+global_info.colors = get_jobs_from_file('job_generator/job_units.txt');
 disp('All jobs loaded');
 
 disp('Sorting jobs on length ...');
 global_info.remaining_units = sort_on_length(global_info.colors);
 disp('Done');
 
-global_info.algorithm = 'rr';
+global_info.algorithm = 'sjf';
 
 pns = pnstruct('generator_pdf');
-dyn.m0 = {'pJobUnits', length(global_info.remaining_units), 'pReady',1}; % pJobbUnits starts with 5 tokens
+dyn.m0 = {'pJobUnits', length(global_info.remaining_units), 'pReady',1};
 dyn.ft = {'tProcess', 0.1, 'tColorizer', 0.1, 'tRemove', 0.1,...
            'tCs', 1, 'allothers', 0.1};
 
