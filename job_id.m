e288ce19e555cd747ee66f0f6c933311857ddc72
sandves <stian@sandve.org>
2015-11-11 11:14:37 +0100

    Merge conflict resolved

--- job_id.m
+++ job_id.m
@@ -1,11 +1,6 @@
 % See the comments in extract_value.m for an
 % explanation of the function.
 function jid = job_id(color)
-<<<<<<< HEAD
-jid = extract_value(color, 'job_id');
-end
-=======
 c = {char(color(1)), char(color(2)), char(color(3)), char(color(4))};
 jid = extract_value(c, 'job_id');
 end
->>>>>>> origin/master
