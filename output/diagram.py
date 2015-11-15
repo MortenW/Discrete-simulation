def383450a3cf16ed197c6de39a455d4e2816994
Morten Wærsland <morten_w92@hotmail.com>
2015-11-15 13:01:48 +0100

    small fixes

--- output/diagram.py
+++ output/diagram.py
@@ -1,22 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.backends.backend_pdf import PdfPages
 
 rr_short = []
 sjf_short = []
 fcfs_short = []
 files = ['rr_result_short.txt','fcfs_result_short.txt','sjf_result_short.txt']
-
+sample = 2
 
 for i, value in enumerate(files):
 	file = open(value, 'r')
 	for j,line in enumerate(file):
 		try:
 			line = float(line)
-			if i == 0 and (j % 2 == 0):
+			if i == 0 and (j % sample == 0):
 				rr_short.append(line)
-			elif i == 1 and (j % 2 == 0):
+			elif i == 1 and (j % sample == 0):
 				fcfs_short.append(line)
-			elif i == 2 and (j % 2 == 0):
+			elif i == 2 and (j % sample == 0):
 				sjf_short.append(line)
 		except ValueError:
 			break
@@ -39,24 +40,26 @@ rects1 = plt.bar(index, fcfs_short, bar_width,
                  color='b',
                  #yerr=std_men,
                  error_kw=error_config,
-                 label='fcfs, avg:175.730675')
+                 label='fcfs, avg:175.73')
 
 rects2 = plt.bar(index + bar_width, sjf_short, bar_width,
                  alpha=opacity,
                  color='y',
                  #yerr=std_men,
                  error_kw=error_config,
-                 label='sjf, avg:228.803775')
+                 label='sjf, avg:228.80')
 rects3 = plt.bar(index +bar_width+bar_width, rr_short, bar_width,
                  alpha=opacity,
                  color='r',
                  #yerr=std_men,
                  error_kw=error_config,
-                 label='rr, avg:282.26485')
+                 label='rr, avg:282.26' )
 plt.xlabel('job id')
 plt.ylabel('execution time')
 plt.legend()
-plt.savefig("diagrma_short_collection.pdf")
-plt.show()
-plt.close()
+
+pp = PdfPages('diagram_short_collection.pdf')
+pp.savefig()
+pp.close()
+
 #plt.tight_layout()
